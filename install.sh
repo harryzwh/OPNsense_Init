a630951e3d435477a90b3c002689ca41317d4b6c
harryzwh <harryzwh@gmail.com>
2019-10-30 01:04:53 +1100

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 DOMAIN=${DOMAIN-"sdn.lab"}
-IP_PREFIX=${IP_PREFIX-"192.168.86"}
+IP_PREFIX=${IP_PREFIX-"192.168.58"}
 
-if [[ ($(uname) == "FreeBSD") ]]; then
-pkg update
-pkg install -y os-vmware os-zerotier git
-fi
+#pkg update
+#pkg install -y os-vmware os-zerotier git
 
 curl -o ~/config-OPNsense.xml -sS https://raw.githubusercontent.com/harryzwh/OPNsense_Init/master/config-OPNsense.xml
+sed -i "s/@domain@/$DOMAIN/" ~/config-OPNsense.xml
+sed -i "s/@IP_Prefix@/$IP_PREFIX/" ~/config-OPNsense.xml
+cp ~/config-OPNsense.xml /conf/config.xml
 
+curl -L https://raw.githubusercontent.com/harryzwh/KMS-on-pfSense/master/kms2pfsense.sh | sh
