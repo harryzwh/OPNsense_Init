765b1cd32f28d38b7923f55895d2832f7694cf73
harryzwh <harryzwh@126.com>
2019-11-07 23:20:45 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,9 @@ pkg update
 pkg install -y os-vmware os-zerotier git
 
 curl -o ~/config-OPNsense.xml -sS https://raw.githubusercontent.com/harryzwh/OPNsense_Init/master/config-OPNsense.xml
-sed -i ".bak" -e "s/@domain@/$DOMAIN/" ~/config-OPNsense.xml
-sed -i ".bak" -e "s/@domain@/$DOMAIN/" ~/config-OPNsense.xml
-sed -i ".bak" -e "s/@IP_Prefix@/$IP_PREFIX/" ~/config-OPNsense.xml
-sed -i ".bak" -e "s/@IP_Gateway@/$GW_IP/" ~/config-OPNsense.xml
+sed -i ".bak" -e "s/@domain@/$DOMAIN/g" ~/config-OPNsense.xml
+sed -i ".bak" -e "s/@IP_Prefix@/$IP_PREFIX/g" ~/config-OPNsense.xml
+sed -i ".bak" -e "s/@IP_Gateway@/$GW_IP/g" ~/config-OPNsense.xml
 cp ~/config-OPNsense.xml /conf/config.xml
 
 curl -L https://raw.githubusercontent.com/harryzwh/KMS-on-pfSense/master/kms2pfsense.sh | sh
